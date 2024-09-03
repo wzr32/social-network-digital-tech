971ac9d6d8a834a6d71b23df70c55b9b3256b6c8
Ruben Padilla <alejandro010492@gmail.com>
2024-09-02 23:43:35 -0400

    import and export post from user account added. fix some minors design details

--- src/pages/private/user-profile/services/user.service.ts
+++ src/pages/private/user-profile/services/user.service.ts
@@ -1,7 +1,22 @@
+import toast from "react-hot-toast";
+
 import { useStore } from "@/store";
 import { Post } from "@/types";
 
-export const getUserPostsService = (): Post[] => {
-	const { getUserPosts, user } = useStore.getState();
-	return getUserPosts(user.username);
+const { getUserPosts, user, createPost } = useStore.getState();
+export const getUserPostsService = (): Post[] => getUserPosts(user.username);
+
+export const createBatchPostsService = (
+	posts: Post[],
+): { success: boolean; error?: unknown } => {
+	try {
+		posts.forEach((post) => {
+			createPost(post);
+		});
+		return { success: true };
+	} catch (error) {
+		toast.error("Error creating posts");
+
+		return { success: false, error };
+	}
 };
